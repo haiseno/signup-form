9f81dfa5b98c22712f9cd9ae9e9bf88a4e22a344
Kenny Zhao <haise.zhao@gmail.com>
2023-01-10 17:36:31 -0800

    Add function to stop form submission
    This change also corrected validation behavior when user switches to a new input field after finishing one.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,22 @@
 const inputs = document.querySelectorAll("input");
 const listItems = document.querySelectorAll(".password-container li");
+const form = document.querySelector("form");
 
 inputs.forEach(input => input.addEventListener("focus", checkValidity));
+form.addEventListener("submit", checkSubmit);
 
+let oldInput;
+let newInput;
+
+let canSubmit;
+
+//Validation behavior based on this thread "https://twitter.com/vponamariov/status/1380182211576664067"
 function checkValidity(e) {
     //Grab onto the input DOM element
     const inputElement = e.composedPath()[0];
+
+    //What is our current input
+    newInput = inputElement.id;
 
     switch (inputElement.id) {
         case "form-first-name":
@@ -14,38 +25,63 @@ function checkValidity(e) {
         case "form-username":
         case "form-password":
         case "form-confirm-password":
+            //Checks validation only when user leaves input
             inputElement.addEventListener("blur", validity);
-            if (userInteract) {
+            //Checks validation while user is focused on input if they had previously interacted with it
+            if (oldInput === newInput && userInteract) {
                 inputElement.addEventListener("input", validity);
             }
+            //Toggles password requirements visual validation
             inputElement.addEventListener("input", passwordCheck);
+            //Updates confirmPassword variable
             inputElement.addEventListener("input", confirmPasswordCheck);
             break;
     }
 }
 
+//Switch-like variable to check whether user has actually already interacted with the input
 let userInteract;
+let inputValid; //This switch-like variable is just for confirmPassword check
 
 function validity(e) {
     const inputElement = e.composedPath()[0];
 
+    //Checks if user has actually interacted with the input
     if (inputElement.value !== "") {
         userInteract = true;
+        oldInput = inputElement.id;
     }
-    else {
+    else { //If not, reset all validation behavior
         userInteract = false;
         inputElement.classList.remove("validity", "invalidity");
         inputElement.removeEventListener("input", validity);
     }
 
+    //Checks input validity and adds appropriate class which will toggle CSS visual indicator
     if (userInteract) {
         if (inputElement.checkValidity()) {
             inputElement.classList.remove("invalidity");
             inputElement.classList.add("validity");
+            inputValid = true; 
         }
         else {
             inputElement.classList.add("invalidity");
             inputElement.classList.remove("validity");
+            inputValid = false;
+        }
+    }
+
+    //Special case for confirm pass input field because it has to match pattern and password
+    if (userInteract && inputElement.id === "form-confirm-password" && inputValid) {
+        if (password === confirmPassword) {
+            inputElement.classList.remove("invalidity");
+            inputElement.classList.add("validity");
+            canSubmit = true;
+        }
+        else {
+            inputElement.classList.add("invalidity");
+            inputElement.classList.remove("validity");
+            canSubmit = false;
         }
     }
 }
@@ -62,6 +98,7 @@ const special = /(?=.*[@$!%*?&])/;
 function passwordCheck(e) {
     const inputElement = e.composedPath()[0];
 
+    //Prevent switch statement fallthrough
     if (inputElement.id !== "form-password") {
         return;
     }
@@ -102,5 +139,25 @@ function togglePasswordValidation(pattern, listItem) {
     }
     else {
         listItem.classList.remove("password-valid");
+    }
+}
+
+//Updates confirmPassword variable
+function confirmPasswordCheck(e) {
+    const inputElement = e.composedPath()[0];
+
+    //Prevent switch statement fallthrough
+    if (inputElement.id !== "form-confirm-password") {
+        return;
+    }
+
+    //Store password input in a string for RegEx match
+    confirmPassword = inputElement.value;
+}
+
+//Stop form submission if passwords do not match
+function checkSubmit(e) {
+    if (!canSubmit) {
+        e.preventDefault();
     }
 }
